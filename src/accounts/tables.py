0ca8878080f936d99a1c951aa182e07e93cd8ca5
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-01-17 21:25:30 +0545

    update: Changes in child tables

--- src/accounts/tables.py
+++ src/accounts/tables.py
@@ -1,23 +1,22 @@
 from django.contrib.auth.hashers import make_password, check_password
 
-from datetime import datetime
 from src.core.db_manager import Table
 
 
 class Users(Table):
     table_name = 'users'
+    _attrs = {
+        "email": str,
+        "phone_number": str,
+        "password": str | None,
+        "is_admin": bool,
+        "is_active": bool,
+    }
     required_fields = ['email', 'password']
         
     def __init__(self):
         super().__init__()
-        self.id = int()
-        self.email = str()
-        self.phone_number = str()
-        self.password = str()
-        self.is_admin = False
-        self.is_active = True
-        self.created_at = None
-        self.updated_at = None
+        self._attrs.update(super()._attrs)
        
     @staticmethod 
     def abstract_method():
@@ -28,38 +27,39 @@ class Users(Table):
         return self.password
     
     def check_password(self, raw_password):
-        password = "get from database"
+        password = "fetch from database"
         return check_password(raw_password, password)
     
     def __str__(self):
-        return (f"id: {self.id} email: {self.email}")
+        return (f"email: {self.email}")
     
     
 class Customers(Table):
     table_name = 'customers'
+    _attrs = {
+        "user_id": int,
+        "first_name": str,
+        "last_name": str,
+        "gender": str,
+        "photo_url": bool
+    }
     required_fields = ['user_id']
     
     def __init__(self):
         super().__init__()
-        self.id = int()
-        self.user_id = int()
-        self.first_name = str()
-        self.last_name = str()
-        self.gender = str()
-        self.photo_url = True
-        self.created_at = None
-        self.updated_at = None
+        self._attrs.update(super()._attrs)
+        self.first_name = 'No name'
        
     @staticmethod 
     def abstract_method():
         pass
     
     def __str__(self):
-        return (f"id: {self.id} Name: {self.first_name} {self.last_name}")
+        return (f"Name: {self.first_name}")
 
 if __name__ == "__main__":
-    # result = Users().get({"email": "abc@test.com"})
-    # print(result)
-    user = Users()
-    col = "is_admin"
-    print(user.created_at)
+    result = Users().get(id='3')
+    if result:
+        print(result.__dict__)
+    else:
+        print(result)
