28ddd1e1c417101f048214402c4987fdff0d437c
LibraAkaja <adhikaribiraj908@gmail.com>
2026-01-26 00:32:39 +0545

    Create a subdir to store different Footer files

---
+++ Frontend/src/components/common/Footer/footerCustomer.js
@@ -0,0 +1,15 @@
+export default {
+    company: [
+        {label: "About CitySewa", path: "/about"},
+        {label: "Browse Services", path: "/services"}
+    ],
+    support: [
+        {label: "My Account", path: "/customer/profile"},
+        {label: "Help Center", path: "/help"},
+        {label: "Contact Support", path: "/contact"}
+    ],
+    legal: [
+        {label: "Privacy Policy", path: "/privacy"},
+        {label: "Terms & Conditions", path: "/terms"}
+    ]
+};
