3417f9c6aefeb20bc0e60e4d3833991b6a566f4a
LibraAkaja <adhikaribiraj908@gmail.com>
2026-01-30 19:53:02 +0545

    Add provider table

--- Frontend/src/pages/dashboard/admin/AdminDashboard.jsx
+++ Frontend/src/pages/dashboard/admin/AdminDashboard.jsx
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import "../../../Style/Dashboard.css";
 import Customers from "./CustomerTable";
+import Providers from "./ProviderTable";
+// import { filteredProviders } from "./ProviderTable";
 import { useOutletContext } from "react-router-dom";
 
-const AdminDashboard = () => {
+const AdminDashboard = (e) => {
+    const [searchBy, setSearchBy] = useState("Id");
+    const [searchText, setSearchText] = useState("");
     const {activeSection} = useOutletContext();
     const renderContext = () => {
         switch(activeSection){
             case "Customers":
-                return <Customers/>
-            // Similarly put cases for other sidebar options
+                return <Customers searchText={searchText} searchBy={searchBy}/>
+            case "Providers":
+                return <Providers searchText={searchText} searchBy={searchBy}/>
             default:
                 return null;
         }
     };
+    const handleChange = (e) => {
+        setSearchBy(e.target.value);
+    }
     return(
         <section className="admin-dashboard">
             <h1>Admin Dashboard</h1>
+            <input type="text" placeholder={`Search by ${searchBy}`} value={searchText} id="searchBar" onChange={(e)=>setSearchText(e.target.value)}/>
+            <select value={searchBy} name="searchBy" onChange={handleChange}>
+                <option disabled hidden value={""}>Search by</option>
+                <option value={"Id"}>Id</option>
+                <option value={"First Name"}>First Name</option>
+                <option value={"Last Name"}>Last Name</option>
+            </select>
             {renderContext()}
         </section>
     );
