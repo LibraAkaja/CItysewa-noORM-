3fa9129bb88ba8aa97f6a502d114908384635a26
LibraAkaja <adhikaribiraj908@gmail.com>
2026-02-01 00:46:20 +0545

    Structural prototype

--- Frontend/src/pages/profiles/ProviderProfile.jsx
+++ Frontend/src/pages/profiles/ProviderProfile.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import "../../Style/Profiles.css";
+import { fetchVerificationData } from "../../api/client";
+import VerificationForm from "./components/VerificationForm";
+import VerificationDetails from "./components/VerificationDetails";
+
 const ProviderProfile = () => {
+    const [loading, setLoading] = useState(true);
+    const [verification, setVerification] = useState(null);
+
+    useEffect(()=>{
+        const loadVerificationData = async () => {
+            try{
+                const res = await fetchVerificationData();
+                setVerification(res.data);
+            } catch(err) {
+                console.log(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        loadVerificationData();
+    }, []);
+
+    if(loading) {
+        return <p>Loading Profile...</p>;
+    }
     return(
-        <h2>Provider Profile</h2>
+        <section className="provider-profile">
+            <h2>My Profile</h2>
+            {!verification?.is_verified ? (<VerificationForm/>) : (<VerificationDetails data={verification}/>)}
+        </section>
     );
 };
 
